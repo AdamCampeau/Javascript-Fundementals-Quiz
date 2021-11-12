f571d5165f87b8d03738b42afdf19b037543fdfb
Adam Campeau <adamc@baytechplastics.com>
2021-11-11 20:49:57 -0500

    set framework for timer

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+
+// Countdown Timer Function 
+
+function startTimer(duration, display){
+    var timer = duration, seconds;
+    setInterval(function () {
+        seconds = parseInt(timer % 60,10);
+
+        if(--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var sixtySeconds = 0,
+        display = document.querySelector('#time');
+    startTimer(sixtySeconds, display);
+}
