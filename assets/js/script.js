9cb543981ebba89e54a8eb6037a2f4008cd01fd9
AdamCampeau <adamcampeau3@gmail.com>
2021-11-13 15:12:27 -0500

    all messages added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,10 @@ var timer = 60;
 // Countdown Timer Function 
 
 // Quiz Questions
+
+
 var questions = [
+//1
     {question: "What is JavaScript?",
     options: ["Java language for web development",
             "Markup Language",
@@ -10,145 +13,137 @@ var questions = [
             "Scripting Language"],
     answer: "Java language for web development"},
 
+//2
     {question: "Strings are ______",
     options: ["Classes",
             "Text",
             "Ids",
             "Rows"],
     answer: "Text"},
-
+//3
     {question: "What is an API?",
     options: ["Software interface connected bewteen devices",	
         "Secure server monitoring software",
         "Apple Product Inc. licensed plug-in",
         "An application program id"],
     answer: "Software interface connected bewteen devices"},
-        
+//4        
     {question:"DOM means:",
     options: ["Document Object Mass",
         "Documented Object Markup",
         "Document Object Model",	
         "Docomented Ordered Mass"],
     answer: "Document Object Model"},
-
+//5
     {question:"What does the DOM do?",
     options: ["Connects web pages to programming/scripting languages",
         "Allows a programming interface to external Document customization",  
         "Represents Document as a logical tree",
         "All of the above"],
     answer: "All of the above"},
-    
+//6    
     {question:"A function is",
     options: ["Run when an event occurs",	
         "Undefined",
         "A single use event",
         "Not available in JavaScript"],
     answer: "Run when an event occurs"},
-
+//7
     {question:"An operator",
     options:["Joins strings and runs tests",
         "Executes math equations",
         "All of the above", 	
         "None of the above"],
     answer: "All of the above"},
-
+//8
     {question:"Conditionals", 
     options:["Are interchanging logic-based code blocks",
         "Are True/False logic-based code blocks",		
         "Are Numeric logic-based code blocks",
         "Are Not logic-based code blocks"],
     answer:"Are True/False logic-based code blocks"},
-
+//9
     {question:"Browser interaction is known as",
     options: ["An occurance",
         "An interaction",
         "A rebase",
         "An event"], 	
     answer:"An event"},
-        
+//10        
     {question:"Event listeners",
     options: ["Listen for browser events",
         "Alerts user on event",
         "Are not compatible with JavaScript",
         "Are only usable once"],
     answer:"Listen for browser events"},
-
+//11
     {question:"addEventListener()",
     options: ["Adds event listener to an event", 	
         "Adds event listener to an element",
         "Adds event listnere to an interacation",
         "Adds event listener to an interface"],
     answer: "Adds event listenet to an event"},
-
+//12
     {question:"JavaScript files are only available externally",
     options: ["True",
             "False"],
     answer: "False"}, 	
-
-
+//13
     {question:"Choose the correct JavaScript link",
     options:["<script src=\"../assets/js/script.js\"></script>", 		
         "<script a href=\"../assets/js/script.js\">/script>",
         "<src script=\"../assets/js/script.js\"></script>",
         "<script src=\">.script.is</script>\""],
     answer:"<script src=\"../assets/js/script.js\"></script>"},
-
-console.log()
-
-Displays browser data	x
-Logs event errors
-Logs user events 
-Logs user commands
-
-15.
-
-window.print()
-
-Prints window 	x
-Is only available in CSS
-Is only available in HTML
-None of the above
-
-
-document.getElementId("quiz").innterHTML="Hello World";
-
-Writes "Hello World" into HTML element id, "quiz" 	x
-Will do nothing, code is written incorrectly
-Writes "quiz" into internal HTML element id, "Hello World"
-None of the above
-
-
-
-Code blocks are
-
-Against best practice
-Statements grouped together 	x
-All of the above
-None of the above
-
-return
-
-Is a line break
-Exits a function 	x
-Is a variable id
-'Enter' key event
-
-JavaScript variables
-
-Cannot be re-declared
-Cannot remain undefined
-Cannot span multiple lines
-Cannot use a hyphen 	x
-
-
-Scopes can be
-
-Declared locally within the function
-Declared globally, outside of function
-Declared withing {} block, and used externally
-ALl of the above		x
-Can Only be declared locally and globally
-
+//14
+    {question:"console.log()",
+    options:["Displays browser data",	
+        "Logs event errors",
+        "Logs user events", 
+        "Logs user commands"],
+    answer:"Displayes browser data"},
+//15
+    {question:"window.print()",
+    options:["Prints window", 	
+        "Is only available in CSS",
+        "Is only available in HTML",
+        "None of the above"],
+    answer:"Prints window"},
+//16
+    {question:" document.getElementId(\"quiz\").innterHTML=\"Hello World\";",
+    options:["Writes \"Hello World\" into HTML element id, \"quiz\"", 	
+        "Will do nothing, code is written incorrectly",
+        "Writes \"quiz\" into internal HTML element id, \"Hello World\"",
+        "None of the above"],
+    answer:"Writes \"Hello World\" into HTML element id, \"quiz\""},
+//17
+    {question:"Code blocks are",
+    options:["Against best practice",
+        "Statements grouped together", 	
+        "All of the above",
+        "None of the above"],
+    answer:"Statements grouped togethor"},
+//18
+    {question:"return",
+    options:["Is a line break",
+        "Exits function", 	
+        "Is a variable id",
+        "Triggers Enter key"],
+    answer:"Exits function"},
+//19
+    {question:"JavaScript variables",
+    options:["Cannot be re-declared",
+        "Cannot remain undefined",
+        "Cannot span multiple lines",
+        "Cannot use a hyphen"], 	
+    answer:"Cannot use a hyphen"},
+//20
+    {question:"Scopes can be",
+    options:["Declared locally",
+        "Declared globally and locally",
+        "Declared withing {} block, globally, and locally",
+        "Declared within a block [],globally, and locally "],
+    answer:"Declared withing {} block, globally, and locally"},
 ];
 
 var index = 0;
